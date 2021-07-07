12efb20c91bf49eacd2a9cdafb0ee164c54d4844
BartoszCiura <bartoszciura8@gmail.com>
2021-07-07 10:32:53 +0200

    Fix bugs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,15 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MapComponent} from './map/map.component';
-import {HttpClientModule} from '@angular/common/http';
 import {ReactiveFormsModule} from '@angular/forms';
 import {NgxMapboxGLModule} from 'ngx-mapbox-gl';
 import {environment} from '../environments/environment';
-import { SightsComponent } from './sights/sights.component';
-import { NavComponent } from './nav/nav.component';
-import {MatTabsModule} from '@angular/material/tabs';
-import { SightsListComponent } from './sights-list/sights-list.component';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
+import {SightsComponent} from './sights/sights.component';
+import {NavComponent} from './nav/nav.component';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {SightsListComponent} from './sights-list/sights-list.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +17,7 @@ import {MatCardModule} from '@angular/material/card';
     MapComponent,
     SightsComponent,
     NavComponent,
+    SightsListComponent
   ],
   imports: [
     BrowserModule,
@@ -29,9 +26,7 @@ import {MatCardModule} from '@angular/material/card';
       accessToken: environment.mapboxToken
     }),
     ReactiveFormsModule,
-    MatTabsModule,
-    MatButtonModule,
-    MatCardModule
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
